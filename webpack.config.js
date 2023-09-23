ec86edb684701317123b4a0d6ba40c908827cacb
Geoffrey Hendrey <geoffrey.hendrey@appdynamics.com>
2023-09-23 09:09:07 -0700

    sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default {
+    devtool: 'source-map',
     entry: {
         main: './src/TemplateProcessor.js'
     },
