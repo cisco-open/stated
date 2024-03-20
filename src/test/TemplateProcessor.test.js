127955e8dc46abe533266e0e911bd1722432cb2d
Sergey Sergeev <sergey.sergeev@appdynamics.com>
2024-03-20 07:25:33 -0700

    Revert "swapi dev to tech migration (#59)"
    This reverts commit 01275b2bc7703901ac3ed08907c195f40df4dcb9.

--- src/test/TemplateProcessor.test.js
+++ src/test/TemplateProcessor.test.js
@@ -2277,21 +2277,21 @@ test("dataChangeCallback on delete op from Snapshot", async () => {
                                 "eye_color": "brown",
                                 "birth_year": "29BBY",
                                 "gender": "male",
-                                "homeworld": "https://swapi.tech/api/planets/22/",
+                                "homeworld": "https://swapi.dev/api/planets/22/",
                                 "films": [
-                                    "https://swapi.tech/api/films/1/",
-                                    "https://swapi.tech/api/films/2/",
-                                    "https://swapi.tech/api/films/3/"
+                                    "https://swapi.dev/api/films/1/",
+                                    "https://swapi.dev/api/films/2/",
+                                    "https://swapi.dev/api/films/3/"
                                 ],
                                 "species": [],
                                 "vehicles": [],
                                 "starships": [
-                                    "https://swapi.tech/api/starships/10/",
-                                    "https://swapi.tech/api/starships/22/"
+                                    "https://swapi.dev/api/starships/10/",
+                                    "https://swapi.dev/api/starships/22/"
                                 ],
                                 "created": "2014-12-10T16:49:14.582000Z",
                                 "edited": "2014-12-20T21:17:50.334000Z",
-                                "url": "https://swapi.tech/api/people/14/"
+                                "url": "https://swapi.dev/api/people/14/"
                             }
                         }
                     }
